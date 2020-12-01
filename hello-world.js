09abc6b0db3a8513d7a83ebeb5dd9553e8316778
William Watson <WilliamPWatson1991@gmail.com>
2020-12-01 17:13:34 -0500

    last hello world test

--- hello-world.js
+++ hello-world.js
@@ -8,3 +8,9 @@ function helloWorldPartTwo(){
     const world = 'world';
     console.log(hello, world);
 }
+//last push test (3)
+function helloWorldPartThree(){
+    const one = 'hello';
+    const two = 'world';
+    console.log(`${one} ${two}`);
+}
